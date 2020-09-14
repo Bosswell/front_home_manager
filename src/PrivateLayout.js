bd35af182e0e1441a785177073cff1fd1d9bfc92
Bosswell <bosswell9@gmail.com>
2020-09-14 13:29:52 +0200

    improved mobile view detection and layout swaping

--- src/PrivateLayout.js
+++ src/PrivateLayout.js
@@ -6,16 +6,35 @@ import { isMobile } from 'react-device-detect';
 
 function PrivateLayout({ children }) {
     const [isOpen, setOpen] = useState(!isMobile);
+    const [isMobileSize, setMobileSize] = useState(isMobile);
 
     useEffect(() => {
         window.scrollTo(0, 0);
     });
 
+    useEffect(() => {
+        manipulateView();
+
+        function manipulateView() {
+            if (window.innerWidth <= 1024 && !isMobileSize) {
+                setMobileSize(true);
+                setOpen(false);
+            } else if (window.innerWidth > 1024 && isMobileSize) {
+                setMobileSize(false);
+                setOpen(true);
+            }
+        }
+
+        window.addEventListener('resize', () => {
+            manipulateView();
+        })
+    }, [isMobileSize])
+
     return (
         <menu>
-            <Menu isOpen={isOpen} setOpen={setOpen} isMobile={isMobile}/>
+            <Menu isOpen={isOpen} setOpen={setOpen} isMobile={isMobileSize}/>
             <aside className={'middle-section'}>
-                <SideMenu isOpen={isOpen} setOpen={setOpen} isMobile={isMobile}/>
+                <SideMenu isOpen={isOpen} setOpen={setOpen} isMobile={isMobileSize}/>
                 <section className={'content'}>
                     { children }
                 </section>
